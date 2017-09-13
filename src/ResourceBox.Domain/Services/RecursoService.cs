25063dc0dbb4e69be6d2355c9c8f6e5bf2264629
Renan Frutuozo <frutuozo29@gmail.com>
2017-09-13 20:02:51 -0300

    Classes de serviço do dominio

---
+++ src/ResourceBox.Domain/Services/RecursoService.cs
@@ -0,0 +1,16 @@
+﻿using ResourceBox.Domain.Entities;
+using ResourceBox.Domain.Interfaces.Repository;
+
+namespace ResourceBox.Domain.Services
+{
+    public class RecursoService : ServiceBase<Recurso>, IRecursoService
+    {
+        private readonly IRecursoRepository recursoRepository;
+
+        public RecursoService(IRecursoRepository recursoRepository) 
+            : base(recursoRepository)
+        {
+            this.recursoRepository = recursoRepository;
+        }
+    }
+}
---
+++ src/ResourceBox.Domain/Services/ServiceBase.cs
@@ -0,0 +1,40 @@
+﻿using ResourceBox.Domain.Interfaces.Repository;
+using System.Collections.Generic;
+
+namespace ResourceBox.Domain.Services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IRepositoryBase<TEntity> repositoryBase;
+
+        public ServiceBase(IRepositoryBase<TEntity> repositoryBase)
+        {
+            this.repositoryBase = repositoryBase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            repositoryBase.Add(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return repositoryBase.GetAll();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return repositoryBase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            repositoryBase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            repositoryBase.Update(obj);
+        }
+    }
+}
