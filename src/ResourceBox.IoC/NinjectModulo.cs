210d1008e78081572d04846d38c1046bc0570df2
Renan Frutuozo <frutuozo29@gmail.com>
2017-09-13 20:56:40 -0300

    Configuração da injeção de dependencia

---
+++ src/ResourceBox.IoC/NinjectModulo.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using ResourceBox.Application.Interfaces;
+using ResourceBox.Application.Services;
+using ResourceBox.Domain.Interfaces.Repository;
+using ResourceBox.Domain.Services;
+using ResourceBox.Infra.Data.Repositories;
+
+namespace ResourceBox.IoC
+{
+    public class NinjectModulo : NinjectModule
+    {
+        public override void Load()
+        {
+            //App
+            Bind<IRecursoAppService>().To<RecursoAppService>();
+
+            //Domain
+            Bind(typeof(IRepositoryBase<>)).To(typeof(RepositoryBase<>));
+            Bind<IRecursoRepository>().To<RecursoRepository>();
+            Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+            Bind<IRecursoService>().To<RecursoService>();          
+        }
+    }
+}
