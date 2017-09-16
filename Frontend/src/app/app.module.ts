b459e919f19590d604a53c2ea941a17b9169fa84
Renan Frutuozo <frutuozo29@gmail.com>
2017-09-15 21:00:48 -0300

    Criação do projeto frontend, configuração das dependencias e criação do menu inicial

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
